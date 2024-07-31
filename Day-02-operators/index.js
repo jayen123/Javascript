9831fce1bf335cbc3114ec2acc40ea5c10011a9c
jayen123 <jayenhasan2003@gmail.com>
2024-08-01 02:58:56 +0600

    Day-02-Operators Complete

---
+++ Day-02-operators/index.js
@@ -0,0 +1,49 @@
+let num1 = 15;
+let num2 = 15;
+console.log(num1 + num2);
+console.log(num2 - num1);
+console.log(num2 * num1);
+console.log(num2 / num1);
+console.log(num2 % num1);
+
+let score = 13;
+
+score += 16
+console.log(score);
+
+score -= 16
+console.log(score);
+
+console.log(num1 > num2);
+
+console.log(num1 < num2);
+
+console.log(num1 >= num2);
+
+console.log(num1 <= num2);
+
+console.log(num1 <= num2);
+
+console.log(num1 <= num2);
+
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+
+if (num1 == num2 && num1 === num2) {
+    console.log(num1, num2);
+}
+
+if (num1 == num2 || num1 === num2) {
+    console.log(num1, num2);
+}
+
+if (num1 != num2) {
+    console.log(num1, num2);
+}
+
+num1 > 0 ? console.log("Positive") : console.log("Negative");
+
+// Arithmetic Operations Script 
+// Comparison And Logical Operator Script
+// Ternary Operator Script 
