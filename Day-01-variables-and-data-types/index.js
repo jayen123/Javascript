8acde9a060576833cebf323fde4015f35ca7256b
jayen123 <jayenhasan2003@gmail.com>
2024-08-01 01:51:35 +0600

    complete day-01 challenge

---
+++ Day-01-variables-and-data-types/index.js
@@ -0,0 +1,35 @@
+var count = 1;
+console.log(count);
+
+let name = "Hasan";
+console.log(name);
+
+const isLoggedIn = false
+console.log(isLoggedIn);
+
+let id = 2;
+console.log(typeof id);
+
+let playerName = "Jayen"
+console.log(typeof playerName);
+
+let verified = true
+console.log(typeof verified);
+
+const myObj = {}
+console.log(typeof myObj);
+
+const myArr = []
+console.log(typeof myArr);
+
+let hobby = "Gardening"
+console.log(hobby);
+
+let passion = hobby = "Swimming"
+console.log(passion);
+
+const countryCode = 123
+
+countryCode = 456
+
+// you can reassign let variable but constant fixed it's not changeable in javascript
